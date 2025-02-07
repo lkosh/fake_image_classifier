54d9353acc773872f1178abb0b89b4dc5c5e101e
Kosheleva Elena <elkosh96@gmail.com>
2025-02-07 11:10:45 +0100

    Update run_model_training.py

--- run_model_training.py
+++ run_model_training.py
@@ -3,27 +3,26 @@ import argparse
 import json
 from trainer import Trainer
 from model_freqnet import  (
-    ArtifactDetector,
-    RealFakeDataset,
-    FocalLoss,
-    AttentionFusion,
-    FeatureProjection
+    ArtifactDetector
 )
 import torch.distributed as dist
+from dataset import RealFakeTripletDataset, RealFakeShuffledDataset
+import traceback
 
 import os
-    # Replace 'your_module' with actual module name
+
 
 class Config:
     def __init__(self):
         self.img_size = 224
         self.batch_size =4
         self.lr = 1e-5
-        self.num_epochs = 10
+        self.num_epochs = 5
+        self.num_epochs_triplet = 15
         self.aux_lambda = 0.1
         # self.vit_model = "WinKawaks/vit-tiny-patch16-224"
         self.vit_model = 'prithivMLmods/Deep-Fake-Detector-v2-Model'
-        self.save_dir = "freqnet_mtcnn_vit_aug_res"
+        self.save_dir = "/root/workspace/research/models/"
         self.debug = False
         self.log_artifacts = True  # Save models and plots locally
         self.common_dim = 256      # Dimension of feature projection for all branches
@@ -31,8 +30,10 @@ class Config:
         self.num_heads = 8
         self.num_layers = 2
         self.dropout=0.1
+        self.margin = 1.0
+        self.lr_triplet = 1e-3
         self.freqnet_weights = 'freqnet_weights.pth'
-
+        self.load_checkpoint = None
     def get(self, key, default=None):
         """
         Retrieve an attribute by name with an optional default value.
@@ -45,6 +46,9 @@ class Config:
             The value of the attribute if it exists; otherwise, the default value.
         """
         return getattr(self, key, default)
+    def set(self, key, value):
+        setattr(self, key, value)
+    
     
     # def to_dict(self):
     #     """
@@ -60,6 +64,9 @@ def main():
     assert torch.cuda.is_available()
     parser = argparse.ArgumentParser(description="Distributed Training with Trainer Class")
     parser.add_argument('--save_dir', type=str, default='freqnet_mtcnn_vit_aug_res', help='Directory to save models and logs.')
+    parser.add_argument('--debug', type=str, default=False, help='Debug mode: tiny dataset')
+    parser.add_argument('--load_checkpoint', type=str, default=None, help='Continue training from a checkpoint')
+    
     args = parser.parse_args()
     
    
@@ -68,6 +75,14 @@ def main():
     # with open(args.config, 'r') as f:
     #     config = json.load(f)
     config = Config()
+    config.set('debug', args.debug)
+
+    if args.debug:
+        config.set('num_epochs_triplet', 2)
+        config.set('num_epochs', 2)
+    if args.load_checkpoint:
+        config.set('load_checkpoint', args.load_checkpoint)
+        
     # Initialize the model
 
     # Initialize the Trainer with distributed flag set based on environment
@@ -96,4 +111,11 @@ def main():
         dist.destroy_process_group()
 
 if __name__ == "__main__":
-    main()
+    try:
+        torch.autograd.set_detect_anomaly(True)
+        main()
+    except Exception as e:
+        traceback.print_exc()
+        if "LOCAL_RANK" in os.environ:
+
+            dist.destroy_process_group()
